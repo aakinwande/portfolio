dc8d6015a10e9721e06c79442014d632959ef643
akinwande <wandeyameen@gmail.com>
2023-01-27 12:58:52 +0100

    Portfolio Project

---
+++ src/components/About.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
+        </div>
+
+        <p className="text-xl mt-15">
+          Akinwande Al-ameen is a fresh graduate of computer engineering who
+          craves for an opportunity to be impactful and gain relevant experience
+          in the IT field. I am highly efficient and an effective team player
+          with demonstrated interests in Software Development, User Interface
+          and User Experience design.
+        </p>
+
+        <br />
+
+        {/* <p className="text-xl">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum
+          pariatur, vel similique sint, nobis aspernatur ut praesentium
+          explicabo ipsam aliquid quasi laboriosam et culpa possimus repudiandae
+          quisquam ullam maiores ab unde. Fugiat odio mollitia nemo alias.
+          Commodi facilis atque nulla vero voluptatem explicabo. Quibusdam,
+          magni quo! Eum cupiditate debitis labore.
+        </p> */}
+      </div>
+    </div>
+  );
+};
+
+export default About;
